9f9c0e7d27e55cb1e9fe84fd5534e994eaa1a0c2
Yordi Colina <64174174+YordiColina@users.noreply.github.com>
2022-02-03 17:24:38 -0500

    the first step

---
+++ android/app/src/main/kotlin/com/YordiColina/buny_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.YordiColina.buny_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:buny_chat/views/signin.dart';
+import 'package:buny_chat/views/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+
+        primarySwatch: Colors.blue,
+      ),
+      home: signup(),
+
+    );
+  }
+}
+
